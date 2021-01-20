1e4c81f9466855f40e033429e57711d15f99e68c
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-01-20 14:17:18 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -15,7 +15,7 @@ JOIN : https://t.me/TGBotsCollection
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = " 📥DownloadinG📥 \n\n Wait untill it completed."
+    DOWNLOAD_START = " 📥DownloadinG📥 \n\n Wait untill it completed. \n"
     UPLOAD_START = " 📤UploadinG📤 "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
