26c13d92ac76e714dc81a79cfb27cab0446408e0
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-01-29 14:14:19 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -16,7 +16,7 @@ JOIN : https://t.me/TGBotsCollection \nFor the List of Telegram Bots
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     DOWNLOAD_FILE = " 📥DownloadinG📥 File "
-    UPLOAD_FILE = " 📥UploadinG📥 \n To www.transfer.sh "
+    UPLOAD_FILE = " 📥UploadinG📥 \n\nTo www.transfer.sh "
     DOWNLOAD_START = " 📥DownloadinG📥 \n\nWait⏳ untill it completed."
     UPLOAD_START = " 📤UploadinG📤 "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
