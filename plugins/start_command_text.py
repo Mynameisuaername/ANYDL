d67e0c75e223386c706a099e69830f706c2f54ff
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-02-01 15:42:23 +0530

    Create start_command_text.py

---
+++ plugins/start_command_text.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
+        [InlineKeyboardButton(
+            "Try", url="https://t.me/TGBotsCollectionbot")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
