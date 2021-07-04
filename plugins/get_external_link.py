dbcdf777f34ef591e0bd6a3387242980b338c201
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-04 15:40:22 +0530

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -100,7 +100,7 @@ async def get_link(bot, update):
             t_response_arry = t_response.decode("UTF-8").split("\n")[-1].strip()
         await bot.edit_message_text(
             chat_id=update.chat.id,
-            text=Translation.AFTER_GET_DL_LINK.format(t_response_arry, max_days),
+            text=Translation.AFTER_GET_LINK.format(t_response_arry, max_days),
             parse_mode="html",
             message_id=a.message_id,
             disable_web_page_preview=True
