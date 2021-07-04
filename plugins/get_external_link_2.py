a831e1f8d8d45f8176d319220c8e426b5e6c186c
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-04 18:07:41 +0530

    Update get_external_link_2.py

--- plugins/get_external_link_2.py
+++ plugins/get_external_link_2.py
@@ -94,10 +94,11 @@ async def get_link(bot, update):
             return False
         else:
             logger.info(t_response)
-            t_response_arry = re.findall("(?P<url>https?://[^\s]+)", t_response)
+            t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
+            t_response_ray = re.findall("(?P<url>https?://[^\s]+)", t_response_array)
         await bot.edit_message_text(
             chat_id=update.chat.id,
-            text=Translation.AFTER_GET_DL_LINK.format(t_response_arry[0], t_response_array[1]),
+            text=Translation.AFTER_GET_DL_LINK.format(t_response_ray[0], t_response_ray[1]),
             parse_mode="html",
             message_id=a.message_id,
             disable_web_page_preview=True
