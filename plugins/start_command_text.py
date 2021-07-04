f08ce04e1b422ad689f4d79e1c7a428da7e0e383
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-04 09:10:27 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -1,5 +1,8 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+import time
 
+bot_start_time = time.time()
+bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time))
 
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
@@ -9,6 +12,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Try", url="https://t.me/TGBotsCollectionbot")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
