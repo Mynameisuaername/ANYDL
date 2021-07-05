dfacc815a5cbe37567b0447fb3d02d4a000e0890
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-05 14:08:29 +0530

    Update get_external_link_3.py

--- plugins/get_external_link_3.py
+++ plugins/get_external_link_3.py
@@ -95,13 +95,16 @@ async def get_link(bot, update):
             logger.info(t_response)
             t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
             #t_response_ray = re.findall("(?P<url>https?://[^\s]+)", t_response_array)
-            t_response_ray = t_response_array.rsplit('"')
+            t_response_ray = t_response_array.rsplit('"') 
+            DO_LINK = InlineKeyboardMarkup([ [InlineKeyboardButton("Download Link", url=t_response_ray[15])], ]) 
         await bot.edit_message_text(
-            chat_id=update.chat.id,
-            text=Translation.AFTER_GET_DL_LINK.format(t_response_ray[25], t_response_ray[-2], t_response_ray[15]),
-            parse_mode="html",
-            message_id=a.message_id,
-            disable_web_page_preview=True
+               chat_id=update.chat.id, 
+
+               text=Translation.AFTER_GET_DL_LINK.format(t_response_ray[25], t_response_ray[-2], t_response_ray[15]), 
+               parse_mode="html", 
+               reply_markup=DO_LINK, 
+               message_id=a.message_id,
+               disable_web_page_preview=True
         )
         try:
             os.remove(after_download_file_name)
