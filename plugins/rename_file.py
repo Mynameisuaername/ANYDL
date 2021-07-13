9e2b0a9fb5105465df5307a94df936e3d3f8ce3f
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-13 21:42:41 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -33,7 +33,7 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["ren"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["ren"]))
 async def rename_doc(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
