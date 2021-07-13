2ffed9f95e1afa6f0ee13922efc7a21619c6cc10
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-13 13:54:32 +0530

    Update get_external_link_2.py

--- plugins/get_external_link_2.py
+++ plugins/get_external_link_2.py
@@ -13,7 +13,6 @@ import os
 import requests
 import subprocess
 import time
-import re
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -28,6 +27,7 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.display_progress import progress_for_pyrogram
+from helper_funcs.ran_text import ran
 
 from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -43,7 +43,7 @@ async def get_link(bot, update):
     logger.info(update.from_user)
     if update.reply_to_message is not None:
         reply_message = update.reply_to_message
-        download_location = Config.DOWNLOAD_LOCATION + "/"
+        download_location = Config.DOWNLOAD_LOCATION + "/" + ran + "/"
         start = datetime.now()
         a = await bot.send_message(
             chat_id=update.chat.id,
@@ -65,20 +65,22 @@ async def get_link(bot, update):
         download_file_name_1 = after_download_file_name.rsplit("/",1)[-1]
         download_file_name = download_file_name_1.rsplit(".",1)[0]
         url = "https://api.anonfiles.com/upload"
-        await bot.edit_message_text(
-            text=Translation.SAVED_RECVD_DOC_FILE,
-            chat_id=update.chat.id,
-            message_id=a.message_id
+        if the_real_download_location is None:
+            await bot.send_message(
+                text=Translation.FILE_NOT_FOUND,
+                chat_id=update.chat.id,
+                reply_to_message_id=update.message_id
         )
-        end_one = datetime.now()
-        command_to_exec = [
-        "curl",
-        "-F", f"file=@\"{after_download_file_name}\"", url
+        else:
+            end_one = datetime.now()
+            command_to_exec = [
+            "curl",
+            "-F", f"file=@\"{after_download_file_name}\"", url
         ]
-        await bot.edit_message_text(
+        up = await bot.send_message(
             text=Translation.ANNO_UPLOAD,
             chat_id=update.chat.id,
-            message_id=a.message_id
+            reply_to_message_id=update.message_id
         )
         try:
             logger.info(command_to_exec)
@@ -88,32 +90,25 @@ async def get_link(bot, update):
             await bot.edit_message_text(
                 chat_id=update.chat.id,
                 text=exc.output.decode("UTF-8"),
-                message_id=a.message_id
+                message_id=up.message_id
             )
             return False
         else:
             logger.info(t_response)
             t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
             #t_response_ray = re.findall("(?P<url>https?://[^\s]+)", t_response_array)
-            t_response_ray = t_response_array.rsplit('"')
-            DO_LINK = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Download Link", url=t_response_ray[11])],
-    ])
-        await bot.edit_message_text(
-            chat_id=update.chat.id,
-            
-            text=Translation.AFTER_GET_DL_LINK.format(t_response_ray[25], t_response_ray[-2], t_response_ray[15]),
-            parse_mode="html",
-            reply_markup=DO_LINK,
-            message_id=a.message_id,
-            disable_web_page_preview=True
+            t_response_ray = t_response_array.rsplit('"') 
+            DO_LINK = InlineKeyboardMarkup([ [InlineKeyboardButton("Download Link", url=t_response_ray[11])], ]) 
+        await bot.send_message(
+               chat_id=update.chat.id, 
+
+               text=Translation.AFTER_GET_DL_LINK.format(t_response_ray[25], t_response_ray[-2], t_response_ray[15]), 
+               parse_mode="html", 
+               reply_markup=DO_LINK, 
+               reply_to_message_id=update.message_id,
+               disable_web_page_preview=True
         )
-        '''await bot.edit_message_reply_markup(
-            chat_id=update.chat.id,
-            message_id=a.message_id,
-            InlineKeyboardMarkup([
-        [InlineKeyboardButton("Download Link", url=t_respone_ray[11])],
-    ]))'''
+        await up.delete()
         try:
             os.remove(after_download_file_name)
         except:
