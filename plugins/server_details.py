92af2a73e0ceb2bea866752c495ef4da33938944
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-13 22:09:24 +0530

    Update server_details.py

--- plugins/server_details.py
+++ plugins/server_details.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, Filters, StopPropagation
+from pyrogram import Client, filters, StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import psutil
 import time
