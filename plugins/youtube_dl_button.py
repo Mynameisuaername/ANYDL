f1e4c47f101af74e98e49d24f6e1dab2ef492def
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-18 12:26:00 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -329,7 +329,7 @@ async def youtube_dl_call_back(bot, update):
                     if os.path.exists(image):
                         if i == 0:
                             media_album_p.append(
-                                pyrogram.InputMediaPhoto(
+                                pyrogram.raw.types.InputMediaPhoto(
                                     media=image,
                                     caption=caption,
                                     parse_mode="html"
@@ -337,7 +337,7 @@ async def youtube_dl_call_back(bot, update):
                             )
                         else:
                             media_album_p.append(
-                                pyrogram.InputMediaPhoto(
+                                pyrogram.raw.types.InputMediaPhoto(
                                     media=image
                                 )
                             )
