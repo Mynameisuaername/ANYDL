3c7fd8f6eb0ab80c365dfb71b0c0a0733b45206f
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-11-26 19:24:44 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -39,9 +39,10 @@ from helper_funcs.ran_text import ran
 
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
-    print(tg_send_type)
+
     # youtube_dl extractors
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("|")
+    print(tg_send_type)
     thumb_image_path = Config.DOWNLOAD_LOCATION + \
         "/" + str(update.from_user.id) + f'{ran}' + ".jpg"
     save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
