1807a04198f7890669e02d8d5c12e41be5425d36
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-12-20 14:02:47 +0530

    Create speddd.py

---
+++ plugins/speddd.py
@@ -0,0 +1,63 @@
+from pyrogram import Client, filters, StopPropagation
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import psutil
+import time
+from speedtest import Speedtest
+
+
+@Client.on_message(filters.command(["Speedtest"]) & filters.privte)
+async def speed(client, message):
+    try:
+        spg = await bot.send_message(text=f'Running speedtest....', chat_id=update.chat.id, reply_to_message_id=update.message_id,)
+    except Exception as er:
+        print(er)
+        spg = await bot.send_message(
+            text=f'Running speedtest....',
+            chat_id=update.message.chat.id,
+            reply_to_message_id=update.message.message_id,
+        )
+    
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    path = (result['share'])
+    try:
+        print(test)
+    except Exception as ere:
+    	print(ere)
+    	pass
+    string_speed = f'''
+<b>Server</b>
+<b>Name:</b> <code>{result['server']['name']}</code>
+<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+    
+<b>SpeedTest Results</b>
+<b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
+<b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
+<b>Ping:</b> <code>{result['ping']} ms</code>
+<b>ISP:</b> <code>{result['client']['isp']}</code>
+'''
+
+    spg.delete()
+    try:
+        update.message_id.reply_photo(path, string_speed, parse_mode=ParseMode.HTML)
+    except Exception as cv:
+    	print("Error ", cv)
+        update.message_id.reply_text(string_speed, parse_mode=ParseMode.HTML)
+
+        
+def speed_convert(size):
+    """Hi human, you can't read bytes?"""
+    power = 2 ** 10
+    zero = 0
+    units = {0: "", 1: "Kb/s", 2: "MB/s", 3: "Gb/s", 4: "Tb/s"}
+    while size > power:
+        size /= power
+        zero += 1
+    #return f"{round(size, 2)} {units[zero]}"        
+
+
