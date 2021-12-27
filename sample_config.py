55714b4414491824b7e8f69f5eefadea7d1629a1
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-12-27 18:19:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -17,7 +17,7 @@ class Config(object):
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 2104534975
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
