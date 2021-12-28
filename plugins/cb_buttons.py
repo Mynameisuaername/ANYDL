e3a514092dbbc72df641b711c9a66956f01ecc0d
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-12-28 20:10:21 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -151,7 +151,7 @@ async def button(bot, update):
             if smze>int(cb_data.split("//")[1]):
                 await update.answer("Video, audio downloaded sucessfully. \n\n Upload starts soon.", show_alert="True")'''
         elif len(os.listdir(download_directory)) == 4:
-            print(os.listdir(download_directory))
+            print(len(os.listdir(download_directory)), os.listdir(download_directory))
             await update.answer("Video & Audio downloaded sucessfully\n\nUploading starts soon. . .")
         elif "N/A" or "None" in cb_data:
             #szze = "N/A"
