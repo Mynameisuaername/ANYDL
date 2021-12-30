2c0f19fcf12a1438c1a047607499ef4036c686ab
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-12-30 10:14:19 +0530

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -32,7 +32,7 @@ from helper_funcs.ran_text import random_char
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["getlink"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["getlink", "getlink1", "getlink2", "getlink3"]))
 async def get_link(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
@@ -73,17 +73,59 @@ async def get_link(bot, update):
             message_id=a.message_id
         )
         end_one = datetime.now()
-        url = "https://transfer.sh/{}.{}".format(str(download_file_name), str(download_extension))
-        max_days = "14"
-        command_to_exec = [
-            "curl",
-            # "-H", 'Max-Downloads: 1',
-            "-H", 'Max-Days: 14', # + max_days + '',
-            "--upload-file", after_download_file_name,
-            url
-        ]
+
+        if "getlink1" in update.text:
+            t_xt=Translation.GO_FILE_UPLOAD
+            command_to_exec = [
+            "curl", "https://api.gofile.io/getServer"
+            ]
+            try:
+                logger.info(command_to_exec)
+                t_response = subprocess.check_output(command_to_exec, stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as exc:
+                logger.info("Status : FAIL", exc.returncode, exc.output)
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=exc.output.decode("UTF-8"),
+                    message_id=a.message_id
+                )
+                return False
+            else:
+                logger.info(t_response)
+                t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
+                t_response_ray = t_response_array.split('"')[9]
+                url= f'''https://{t_response_ray}.gofile.io/uploadFile'''
+                command_to_exec=[
+                    "curl",
+                    "-F", f"file=@\"{after_download_file_name}\"", url
+                ]
+        if "getlink2" in update.text:
+            t_xt=Translation.ANNO_UPLOAD
+            url = "https://api.anonfiles.com/upload"
+            command_to_exec=[
+                "curl",
+                "-F", f"file=@\"{after_download_file_name}\"", url
+            ]
+        if "getlink3" in update.text:
+            t_xt=Translation.BAY_UPLOAD
+            url = "https://api.bayfiles.com/upload"
+            command_to_exec=[
+                "curl",
+                "-F", f"file=@\"{after_download_file_name}\"", url
+            ]
+        if "/getlink" == update.text:
+            t_xt=Translation.UPLOAD_FILE
+            url = "https://transfer.sh/{}.{}".format(str(download_file_name), str(download_extension))
+            max_days = "14"
+            command_to_exec = [
+                "curl",
+                # "-H", 'Max-Downloads: 1',
+                "-H", 'Max-Days: 14', # + max_days + '',
+                "--upload-file", after_download_file_name,
+                url
+            ]
         await bot.edit_message_text(
-            text=Translation.UPLOAD_FILE,
+            text=t_xt,
             chat_id=update.chat.id,
             message_id=a.message_id
         )
@@ -100,21 +142,34 @@ async def get_link(bot, update):
             return False
         else:
             logger.info(t_response)
-            t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
+            if "/getlink" == update.text
+                t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
+            else:
+                t_response_array = t_response.decode("UTF-8").split("\n")[-1].strip()
+                t_response_ray = t_response_array.rsplit('"')
             #t_response_ray = re.findall("(?P<url>https?://[^\s]+)", t_response_array)
-            t_response_ray = t_response_array.rsplit()
-            DO_LINK = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Download Link", url=t_response_array)],
-    ])
-        await bot.edit_message_text(
-            chat_id=update.chat.id,
-            
-            text=Translation.AFTER_GET_DL_LINK.format(download_file_name_1, s0ze, t_response_array),
-            parse_mode="html",
-            reply_markup=DO_LINK,
-            message_id=a.message_id,
-            disable_web_page_preview=True
-        )
+
+            if "getlink1" in update.text:
+                DO_LINK=InlineKeyboardMarkup([[InlineKeyboardButton("Download Link", url=t_response_ray[37])], ])
+                t_xt=Translation.AFTER_GET_GOFILE_LINK.format(t_response_ray[29], humanbytes(s0ze), t_response_ray[33], t_response_ray[13])
+            if "getlink2" in update.text:
+                DO_LINK=InlineKeyboardMarkup([[InlineKeyboardButton("Download Link", url=t_response_ray[11])], ])
+                t_xt=Translation.AFTER_GET_LINK.format(t_response_ray[25], t_response_ray[-2], t_response_ray[15])
+            if "getlink3" in update.text:
+                DO_LINK=InlineKeyboardMarkup([[InlineKeyboardButton("Download Link", url=t_response_ray[11])], ])
+                t_xt=Translation.AFTER_GET_LINK.format(t_response_ray[25], t_response_ray[-2], t_response_ray[15])
+            if "/getlink" == update.text:
+                DO_LINK=InlineKeyboardMarkup([[InlineKeyboardButton("Download Link", url=t_response_array)], ])
+                t_xt=Translation.AFTER_GET_DL_LINK.format(download_file_name_1, s0ze, t_response_array)
+
+            await bot.edit_message_text(
+                chat_id=update.chat.id,
+                text=t_xt,
+                parse_mode="html",
+                reply_markup=DO_LINK,
+                message_id=a.message_id,
+                disable_web_page_preview=True
+            )
         try:
             os.remove(after_download_file_name)
             shutil.rmtree(download_location)
@@ -122,7 +177,7 @@ async def get_link(bot, update):
             pass
     else:
         await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.REPLY_TO_DOC_GET_LINK,
-            reply_to_message_id=update.message_id
+        chat_id=update.chat.id,
+        text=Translation.REPLY_TO_DOC_GET_LINK,
+        reply_to_message_id=update.message_id
         )
