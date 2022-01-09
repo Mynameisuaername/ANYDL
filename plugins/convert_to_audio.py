313f92812cf37bcda402494c1ba327b7c02490ad
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2022-01-09 23:22:04 +0530

    Update convert_to_audio.py

--- plugins/convert_to_audio.py
+++ plugins/convert_to_audio.py
@@ -27,6 +27,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.display_progress import progress_for_pyrogram
 from helper_funcs.ran_text import random_char
+from helper_funcs.help_Nekmo_ffmpeg import exa_audio
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -63,70 +64,46 @@ async def convert_to_audio(bot, update):
             )
         )
         if the_real_download_location is not None:
-            a=await bot.edit_message_text(
+            await bot.edit_message_text(
                 text=f"Video Download Successfully, now trying to convert into Audio. \n\n⌛️Wait for some time.",
                 chat_id=update.chat.id,
                 message_id=ab.message_id
             )
+            time.sleep(0.5)
+            auddio=exa_audio(the_real_download_location)
+            if auddio is not None:
+                await bot.edit_message_text(
+                    text=Translation.UPLOAD_START,
+                    chat_id=update.chat.id,
+                    message_id=ab.message_id
+                )
+                metadata = extractMetadata(createParser(auddio))
+                duration=None
+                if metadata.has('duration'):
+                    duration=metadat.get("duration").seconds
+                await bot.send_audio(
+                    chat_id=update.chat.id,
+                    audio=auddio,
+                    supports_streaming=True,
+                    reply_to_message_id=update.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        ab,
+                        c_time
+                    )
+                )
             # don't care about the extension
             # convert video to audio format
-            f_name = the_real_download_location.rsplit('/',1)[-1]
+            '''f_name = the_real_download_location.rsplit('/',1)[-1]
             clip = pp.VideoFileClip(the_real_download_location)
             clip.audio.write_audiofile(f_name+'.mp3')
             audio_file_location = f_name+'.mp3'
-            logger.info(audio_file_location)
+            logger.info(audio_file_location)'''
             # get the correct width, height, and duration for videos greater than 10MB
             # ref: message from @BotSupport
-            metadata = extractMetadata(createParser(audio_file_location))
-            if metadata.has("duration"):
-                duration = metadata.get('duration').seconds
-            '''thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-            if not os.path.exists(thumb_image_path):
-                thumb_image_path = None
-            else:
-                metadata = extractMetadata(createParser(thumb_image_path))
-                if metadata.has("width"):
-                    width = metadata.get("width")
-                if metadata.has("height"):
-                    height = metadata.get("height")
-                # get the correct width, height, and duration for videos greater than 10MB
-                # resize image
-                # ref: https://t.me/PyrogramChat/44663
-                # https://stackoverflow.com/a/21669827/4723940
-                Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
-                img = Image.open(thumb_image_path)
-                # https://stackoverflow.com/a/37631799/4723940
-                # img.thumbnail((90, 90))
-                img.resize((90, height))
-                img.save(thumb_image_path, "JPEG")'''
-                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            # try to upload file
-            await a.delete()
-            c_time = time.time()
-            
-            up=await bot.send_message(
-            text=Translation.UPLOAD_START,
-            chat_id=update.chat.id,
-            )
-
-            c_time = time.time()
-            await bot.send_audio(
-                chat_id=update.chat.id,
-                audio=audio_file_location,
-                duration=duration,
-                # performer="",
-                # title="",
-                # reply_markup=reply_markup,
-                reply_to_message_id=update.reply_to_message.message_id,
-                progress=progress_for_pyrogram,
-                progress_args=(
-                    Translation.UPLOAD_START,
-                    up, 
-                    c_time
-                )
-            )
             try:
-                os.remove(thumb_image_path)
+                # os.remove(thumb_image_path)
                 os.remove(the_real_download_location)
                 os.remove(audio_file_location)
             except:
@@ -134,7 +111,7 @@ async def convert_to_audio(bot, update):
             await bot.edit_message_text(
                 text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
                 chat_id=update.chat.id,
-                message_id=up.message_id,
+                message_id=ab.message_id,
                 disable_web_page_preview=True
             )
     else:
