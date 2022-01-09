e1f9d1bcc4ab57594b5885e7a7866b85ce3b1db4
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2022-01-09 12:37:52 +0530

    Update help_Nekmo_ffmpeg.py

--- helper_funcs/help_Nekmo_ffmpeg.py
+++ helper_funcs/help_Nekmo_ffmpeg.py
@@ -104,16 +104,16 @@ async def cult_small_video(video_file, output_directory, start_time, end_time):
         "/" + str(round(time.time())) + ".mp4"
     file_genertor_command = [
         "ffmpeg",
-        "-i",
-        video_file,
         "-ss",
         start_time,
+        "-i"
+        video_file,
         "-to",
         end_time,
-        "-async",
-        "1",
-        "-strict",
-        "-2",
+        "-c:v",
+        "copy",
+        "-c:a",
+        "copy",
         out_put_file_name
     ]
     process = await asyncio.create_subprocess_exec(
