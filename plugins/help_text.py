b4ea0d2b83e0da3c4e997876822962ba4fdcd987
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2022-01-22 07:27:18 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -47,7 +47,7 @@ async def start(client, message):
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
   
-@Client.on_message(pyrogram.filters.command(["help", "about"]))
+@Client.on_message(pyrogram.filters.command(["help"]))
 async def help_user(bot, update):
     # logger.info(update)
     await bot.send_message(
